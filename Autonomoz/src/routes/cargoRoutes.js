28432bc40e4882f0889eafd125701b9e6d900092
Mônica Cotrim Manfrinato <monica.manfrido@aluno.senai.br>
2026-08-17 08:28:48 -0300

    🌟- Criação dos arquivos base para cargo

---
+++ Autonomoz/src/routes/cargoRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const cargoController = require('../controllers/cargoController');
+
+router.get('/', cargoController.getAll.bind(cargoController));
+router.get('/:id', cargoController.getById.bind(cargoController));
+router.post('/', cargoController.create.bind(cargoController));
+router.put('/:id', cargoController.update.bind(cargoController));
+router.delete('/:id', cargoController.delete.bind(cargoController));
+
+module.exports = router;
